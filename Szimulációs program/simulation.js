18a8f81b2ad49df651fa9e16ce32ad0f2d406bad
MacCroat <aron.solyom@gmail.com>
2024-07-02 14:27:47 +0200

    Update simulation.js

--- Szimulációs program/simulation.js
+++ Szimulációs program/simulation.js
@@ -166,6 +166,147 @@ document.addEventListener('imagesLoaded', () => {
         }
 
         requestAnimationFrame(update);
+<<<<<<< Updated upstream
+=======
+    } else {
+        alert("Kérem, adjon meg érvényes számot mindkét mezőben!");
+    }
+});
+
+function growPlant() {
+    const newPlant = new Entity(Math.random() * canvasWidth, Math.random() * canvasHeight, 'plant');
+    entities.push(newPlant);
+}
+
+setInterval(growPlant, plantGrowthInterval);
+
+function updateCounts() {
+    const herbivoreCountElement = document.getElementById('herbivoreCount');
+    const carnivoreCountElement = document.getElementById('carnivoreCount');
+
+    let herbivoreCount = 0;
+    let carnivoreCount = 0;
+
+    entities.forEach(entity => {
+        if (entity.type === 'herbivore' || entity.type === 'gazelle') {
+            herbivoreCount++;
+        } else if (entity.type === 'carnivore') {
+            carnivoreCount++;
+        }
+    });
+
+    herbivoreCountElement.textContent = `Növényevők (zebrák,gazellák): ${herbivoreCount}`;
+    carnivoreCountElement.textContent = `Ragadozók (oroszlánok): ${carnivoreCount}`;
+}
+
+function getHerbivoreCount() {
+    let count = 0;
+    entities.forEach(entity => {
+        if (entity.type === 'herbivore' || entity.type === 'gazelle') {
+            count++;
+        }
+    });
+    return count;
+}
+
+function getCarnivoreCount() {
+    let count = 0;
+    entities.forEach(entity => {
+        if (entity.type === 'carnivore') {
+            count++;
+        }
+    });
+    return count;
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    let herbivoreCount = 0;
+    let carnivoreCount = 0;
+    let plantCount = 0;
+
+    for (let i = entities.length - 1; i >= 0; i--) {
+        const entity = entities[i];
+        entity.move();
+        entity.draw();
+
+        if (entity.type === 'herbivore' || entity.type === 'gazelle') {
+            herbivoreCount++;
+        } else if (entity.type === 'carnivore') {
+            carnivoreCount++;
+        } else if (entity.type === 'plant') {
+            plantCount++;
+        }
+
+        if (entity.type !== 'plant' && entity.type !== 'rock' && entity.hasStarved()) {
+            entities.splice(i, 1);
+            addMessage(new Date().toLocaleTimeString() + ` Egy ${entity.type === 'herbivore' ? 'zebra' : 'oroszlán' ||
+                entity.type === 'gazelle' ? 'gazella' : 'oroszlán'} éhen halt.`);
+            continue;
+        }
+
+        for (let j = entities.length - 1; j >= 0; j--) {
+            if (i !== j) {
+                const other = entities[j];
+                if (entity.x < other.x + other.size &&
+                    entity.x + entity.size > other.x &&
+                    entity.y < other.y + other.size &&
+                    entity.y + entity.size > other.y) {
+
+                    if ((entity.type === 'herbivore' || entity.type === 'gazelle') && other.type === 'plant') {
+                        entities.splice(j, 1);
+                        entity.updateLastAte();
+                        addMessage(new Date().toLocaleTimeString() + ` Egy 
+                        ${entity.type === 'herbivore' ? 'zebra' : 'gazella'} megevett egy füvet.`);
+                    } else if (entity.type === 'carnivore' && (other.type === 'herbivore' || other.type === 'gazelle')) {
+                        entities.splice(j, 1);
+                        entity.updateLastAte();
+                        addMessage(new Date().toLocaleTimeString() + ` Egy oroszlán megevett egy 
+                        ${entity.type === 'herbivore' ? 'zebrát' : 'gazellát'}.`);
+
+                    } else if (entity.type === 'herbivore' && other.type === 'herbivore') {
+                        entity.meetCounter++;
+                        other.meetCounter++;
+                        if (entity.meetCounter >= 50 && other.meetCounter >= 50) {
+                            if (getHerbivoreCount() < MAX_HERBIVORE_COUNT) {
+                                const newHerbivore = new Entity(Math.random() * canvasWidth, Math.random() * canvasHeight, 'herbivore');
+                                entities.push(newHerbivore);
+                                entity.meetCounter = 0;
+                                other.meetCounter = 0;
+                                addMessage(new Date().toLocaleTimeString() + " A zebrák szaporodtak.");
+                            }
+                        }
+
+                    } else if (entity.type === 'gazelle' && other.type === 'gazelle') {
+                        entity.meetCounter++;
+                        other.meetCounter++;
+                        if (entity.meetCounter >= 50 && other.meetCounter >= 50) {
+                            if (getHerbivoreCount() < MAX_HERBIVORE_COUNT) {
+                                const newGazelle = new Entity(Math.random() * canvasWidth, Math.random() * canvasHeight, 'gazelle');
+                                entities.push(newGazelle);
+                                entity.meetCounter = 0;
+                                other.meetCounter = 0;
+                                addMessage(new Date().toLocaleTimeString() + " A gazellák szaporodtak.");
+                            }
+                        }
+                    }
+                    else if (entity.type === 'carnivore' && other.type === 'carnivore') {
+                        entity.meetCounter++;
+                        other.meetCounter++;
+                        if (entity.meetCounter >= 200 && other.meetCounter >= 200 && getCarnivoreCount() < MAX_CARNIVORE_COUNT) {
+                            const newCarnivore = new Entity(Math.random() * canvasWidth, Math.random() * canvasHeight, 'carnivore');
+                            entities.push(newCarnivore);
+                            entity.meetCounter = 0;
+                            other.meetCounter = 0;
+                            addMessage(new Date().toLocaleTimeString() + " Az oroszlánok szaporodtak.");
+                        }
+                    }
+                }
+            }
+        }
+        updateCounts();
+>>>>>>> Stashed changes
     }
 
     // Eseménykezelők beállítása
